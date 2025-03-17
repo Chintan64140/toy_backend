00f6210c63bde263f97a2c0d7d6b190f32b77a2d
chintan6414 <chintan6414@gmail.com>
2025-03-17 14:31:55 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -11,6 +11,10 @@ app.use("/api/users", router);
 app.use("/api/products", productsRouter);
 app.use("/api/file", uploadRouter);
 
+app.get('/', (res) => {
+    console.log('IS_____CALLES')
+    return res.json("helloss")
+})
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
