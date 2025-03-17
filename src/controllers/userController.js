9ee1e0c5d88ae7a21b0ef0e53b546bf930ad4f12
chintan6414 <chintan6414@gmail.com>
2025-03-18 01:05:31 +0530

    Update code

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -12,6 +12,7 @@ export const getUsers = async (req, res) => {
 };
 
 export const createUser = async (req, res) => {
+  
   const { name, email, password, userName, address, bankAccount } = req.body;
   if (name && email && password && userName && address && bankAccount) {
     const findUser = await prisma.user.findUnique(
