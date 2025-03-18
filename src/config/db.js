b5c14de9d9fc29885a041d6e82219784e9cd2169
chintan6414 <chintan6414@gmail.com>
2025-03-18 23:18:12 +0530

    Update db.js

--- src/config/db.js
+++ src/config/db.js
@@ -1,5 +1,7 @@
 // import { PrismaClient } from "@prisma/client";
-import { PrismaClient } from '../prisma/generated/clientPg'
+// import { PrismaClient } from '../prisma/generated/clientPg'
+
+import { PrismaClient } from "@prisma/client/extension";
 
 const prisma = new PrismaClient();
 
