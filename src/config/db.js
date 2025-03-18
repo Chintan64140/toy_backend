b06787ec07dc38360643f1a8fb975589d8378327
chintan6414 <chintan6414@gmail.com>
2025-03-18 23:03:55 +0530

    Update db.js

--- src/config/db.js
+++ src/config/db.js
@@ -1,4 +1,6 @@
-import { PrismaClient } from "@prisma/client";
+// import { PrismaClient } from "@prisma/client";
+
+import { PrismaClient } from "@prisma/client/scripts/default-deno-edge.js";
 
 const prisma = new PrismaClient();
 
